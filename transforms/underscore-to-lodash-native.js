36acef75f6e2c6d2fb1da8eeb060ef46bc77c411
Josh Hoffman <josh@mixmax.com>
2020-05-06 14:27:44 -0700

    add list of mappings

--- transforms/underscore-to-lodash-native.js
+++ transforms/underscore-to-lodash-native.js
@@ -177,10 +177,65 @@ function remapMethodNameIfNoDirectMatch(methodName) {
   //JAH TODO: go look at notes on .compact from dm w/ Jane
 
   switch (methodName) {
+    case "any":
+      return "some";
+    case "all":
+      return "every";
+    case "compose":
+      return "flowRight";
+    case "contains":
+      return "includes";
+    // Underscore _.each doesn’t allow exiting by returning false is Lodash _.forEach
+    // Underscore _.escape escapes backtick characters ('`'), while Lodash does not
     case "findWhere":
       return "find";
+    // Underscore _.flatten is deep by default while Lodash is shallow
+    // Underscore _.groupBy's iteratee receives the arguments value, indexNumber, and originalCollection, while Lodash _.groupBy's iteratee receives only the argument value
+
+    // Underscore _.indexOf with 3rd parameter undefined is Lodash _.indexOf
+    // Underscore _.indexOf with 3rd parameter true is Lodash _.sortedIndexOf
+    case "indexBy":
+      return "keyBy";
+    case "invoke":
+      return "invokeMap";
+    case "mapObject":
+      return "mapValues";
+    // Underscore _.max combines Lodash _.max & _.maxBy
+    // We will have to figure out argument count and choose max or maxBy accordingly
+    // Underscore _.min combines Lodash _.min & _.minBy
+    // same tbh
+    case "invoke":
+      return "invokeMap";
+    case "mapObject":
+      return "mapValues";
+    // Underscore _.sample combines Lodash _.sample & _.sampleSize
+    // Underscore _.object combines Lodash _.fromPairs and _.zipObject
+    // Underscore _.omit by a predicate is Lodash _.omitBy
+    case "omit":
+      return "omitBy";
+    case "pairs":
+      return "toPairs";
+    case "pick":
+      return "pickBy";
     case "pluck":
       return "map";
+    // Underscore _.uniq by an iteratee is Lodash _.uniqBy. _.uniq with the isSorted parameter = true is Lodash _.sortedUniq (or Lodash _.sortedUniqBy when using an iteree).
+    case "where":
+      return "filter";
+    // Underscore _.isFinite doesn’t align with Number.isFinite
+    // (e.g. _.isFinite('1') returns true in Underscore but false in Lodash)
+    // Underscore _.matches shorthand doesn’t support deep comparisons
+    // (e.g. _.filter(objects, { 'a': { 'b': 'c' } }))
+    // Underscore ≥ 1.7 & Lodash _.template syntax is
+    // _.template(string, option)(data)
+    // Lodash _.memoize caches are Map like objects
+
+    // Lodash doesn’t support a context argument for many methods in favor of _.bind
+    // Lodash supports implicit chaining, lazy chaining, & shortcut fusion
+
+    // Lodash split its overloaded _.head, _.last, _.rest, & _.initial out into
+    // _.take, _.takeRight, _.drop, & _.dropRight
+    // (i.e. _.head(array, 2) in Underscore is _.take(array, 2) in Lodash)
     default:
       return methodName;
   }
@@ -193,7 +248,7 @@ function transformUnderscoreMethod(j, ast) {
     ast.node.callee.property.name
   );
 
-    j.__methods[methodName] = true;
+  j.__methods[methodName] = true;
 }
 
 function transformRequire(j, options) {
