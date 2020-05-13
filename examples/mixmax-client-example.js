4ef56f2890f1bc5a787282c69be854f994b0b9ca
Jane Kim <janek@mixmax.com>
2020-05-13 16:56:25 -0400

    add example for client-side file

---
+++ examples/mixmax-client-example.js
@@ -0,0 +1,27 @@
+import _ from "underscore"; // a comment to preserve
+// this stays
+import moment from "moment-timezone";
+
+// underscore.first => lodash.first
+const firstItem = _.first([1, 2, 3]);
+
+// underscore.contains => lodash.includes
+const arrayHasElem = _.contains([400, 401], 200);
+
+// underscore.max with two arguments => lodash.maxBy
+const mostMembers = _.max(
+  [{ members: [] }, { members: [1, 2, 3] }, { members: [1, 2] }],
+  ({ members }) => members.length
+);
+
+// underscore.max with one argument => lodash.max
+const biggestValue = _.max([1, "a", 1000]);
+
+// underscore.flatten => lodash.flattenDeep
+const flattenedDeep = _.flatten([1, [1, [1]]]);
+
+// underscore.flatten with second arg shallow = true => lodash.flatten
+const flattenedShallow = _.flatten([1, [1, [1]]], true);
+
+// underscore.map => native array map
+const users = _.map(["id1", "id2"], (userId) => ({ userId }));
